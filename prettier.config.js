8980e357ce44726cbdf94246a47dac234e741dfe
smk950226 <smk950226@naver.com>
2025-08-28 17:11:35 +0900

    arrange default

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    tabWidth: 4,
+};
+
+export default config;
